84812a2d284003e452d845e9324d8b604af01357
elj-Wissal <122487965+elj-Wissal@users.noreply.github.com>
2023-04-03 13:29:28 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BasicExample from './FormBoot';
+
+function App() {
+  return (
+    <>
+    <>
+    
+    </>
+    <>
+
+    </>
+    <BasicExample />
+    
+    </>
+  );
+}
+
+export default App;
